e5af36a6423f619df8a1a0a29b4697cc6421a4a6
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2024-06-05 18:04:20 +0300

    wip

---
+++ Bot/Main.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Nodes;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Weather_Telegram_Bot.Bot;
+using Telegram.Bot.Exceptions;
+
+namespace Weather_Discord_Bot
+{
+    class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+        async Task MainAsync()
+        {
+            var bot = new BotEngine();
+            bot.Connect();
+        }
+    }
+}
---
+++ Bot/Tokens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Telegram_Bot.Bot
+{
+    internal class Tokens
+    {
+        public string TelegramToken { get; set; }
+        public string WeatherToken { get; set; }
+    }
+}
